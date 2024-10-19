3f4f3cdea43e7c76f757a09b070ea73cccddf883
SasaRussia <sokolovsasa749@gmail.com>
2024-10-19 18:29:25 +0300

    апишка

---
+++ DjangoAPI/DjangoAPI.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce_db',
+        'USER': 'postgres',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
